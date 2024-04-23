701e1275634592fa362b501cb1bc916c9cbf0383
Gresh <30456475+greshma007@users.noreply.github.com>
2024-04-23 16:41:56 +0530

    initial commit

---
+++ src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-reservation-list',
+  templateUrl: './reservation-list.component.html',
+  styleUrl: './reservation-list.component.css',
+})
+export class ReservationListComponent {}
